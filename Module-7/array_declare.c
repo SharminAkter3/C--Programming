59d5775d8a66cfa5706840eea3d2dbcabcf5c7a3
SharminAkter3 <mssharmin4145@gmail.com>
2023-04-12 14:25:32 +0600

    module 3-7 added

---
+++ Module-7/array_declare.c
@@ -0,0 +1,13 @@
+/*
+basic syntax of array
+ data_type name [size]
+ int array [10]
+ => ak ta int memory ty 4 byte jaiga nei
+*/
+
+#include <stdio.h>
+int main()
+{
+    int arr[10];
+    return 0;
+}
